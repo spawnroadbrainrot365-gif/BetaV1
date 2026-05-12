52d6049923367bb85c12c67771900de685848f76
spawnroadbrainrot365-gif <spawnroadbrainrot365@gmail.com>
2026-05-12 16:53:29 +0300

    Update BetaV1.lua

--- BetaV1.lua
+++ BetaV1.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://local-production-817c.up.railway.app/raw/1778588368675-129068678.lua"))()
+loadstring(game:HttpGet("https://local-production-817c.up.railway.app/raw/1778593954396-316912328.lua"))()
